7e481821d86c030a1e2fe81e61030c6b6bee57a8
DennisK <dennisk@zainahtech.com>
2024-01-31 17:25:54 +0000

    Removed py project

--- sambaapi/sambaapi/doctype/samba_error_logs/samba_error_logs.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, dev@opensource and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class SambaErrorLogs(Document):
-	pass
--- sambaapi/sambaapi/doctype/samba_error_logs/test_samba_error_logs.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, dev@opensource and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSambaErrorLogs(FrappeTestCase):
-	pass
